a1d11495559cae3468b4dd5748e3d83673c013a9
akshaymittur <akshaymittur@gmail.com>
2020-01-11 18:15:23 +0530

    Added Return

--- bot.py
+++ bot.py
@@ -2,9 +2,14 @@ import praw
 import config
 
 def bot_login:
-    praw.Reddit(username = config.username,
-                password = config.password,
-                client_id = config.client_id,
-                client_secret = config.client_secret,
-                user_agent = "Finder App for wiki and youtube")
+    r = praw.Reddit(username = config.username,
+            password = config.password,
+            client_id = config.client_id,
+            client_secret = config.client_secret,
+            user_agent = "Finder App for wiki and youtube")
+    return r
 
+def run_bot(r):
+
+
+r = bot_login()
