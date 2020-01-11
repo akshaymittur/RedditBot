735229d36f07329f17a9c2d28ad1a067f518f12e
akshaymittur <akshaymittur@gmail.com>
2020-01-11 18:35:25 +0530

    Created Basic Bot

--- bot.py
+++ bot.py
@@ -1,20 +1,30 @@
 import praw
 import config
+import time
 
-wiki = "www.wikipedia.org"
+wiki = "wikipedia.org"
 find = "!find"
 
-def bot_login:
+def bot_login():
+    print("Logging In")
     r = praw.Reddit(username = config.username,
             password = config.password,
             client_id = config.client_id,
             client_secret = config.client_secret,
             user_agent = "Finder App for wiki and youtube")
+    print("Logged In")
     return r
 
 def run_bot(r):
+    print("Obtaining Comments")
     for comment in r.subreddit("test").comments(limit=25):
-        if  wiki in comment.body:
+        if wiki in comment.body:
             print ("FOUND")
+            comment.reply("Found!")
+            print("Replied to Comment " + comment.id)
+    print("Sleeping for 10 Seconds")
+    #Sleeping for 10 Seconds
+    time.sleep(10)
 
 r = bot_login()
+run_bot(r)
